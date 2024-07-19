269fe2791fc5c05b24ac23028c0287a04b5f6257
Joseph Szot <38385215+jsphszot@users.noreply.github.com>
2024-07-19 23:12:51 +0200

    Update streamlit_app.py
    testing pandas table iloc

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ my_dataframe=session\
 # st.stop()
 # convert snowpark df to pandas df to use LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
@@ -39,6 +39,8 @@ if ingredients_list:
   
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write(f'The search value for {fruit_chosen} is {search_on}.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response=requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
