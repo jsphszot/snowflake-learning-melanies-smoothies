d9abb0b1e8e88f9ce1242199278dd8b3d6e862dc
Joseph Szot <38385215+jsphszot@users.noreply.github.com>
2024-07-19 22:42:18 +0200

    Update streamlit_app.py
    put json into a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,8 @@ if ingredients_list:
 # NEW SECTION TO DISPLAY FRUITYVICE NUTRITION INFO
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermetlo")
-st.text(fruityvice_response)
+# st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
